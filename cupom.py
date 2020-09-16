c63a7ce679a446a54d3229f26efeade0f765d9fe
antonia <antoniaacaabral@gmail.com>
2020-09-16 15:22:28 -0300

    Update cupom

--- cupom.py
+++ cupom.py
@@ -1,3 +1,23 @@
 def imprime_dados_loja():
     # Implemente aqui
-    return "" 
+    nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
+    logradouro = "Av. Projetada Leste"
+    numero = 500
+    complemento = "EUC F32/33/34"
+    bairro = "Br. Sta Genebra"
+    municipio = "Campinas"
+    estado = "SP"
+    cep = "13080-395"
+    telefone = "(19) 3756-7408"
+    observacao = "Loja 1317 (PDP)"
+    cnpj = "42.591.651/0797-34"
+    inscricao_estadual = "244.898.500.113"
+
+    return f'''{nome_loja}
+{logradouro}, {numero} {complemento}
+{bairro} - {municipio} - {estado}
+CEP:{cep} Tel {telefone}
+{observacao}
+CNPJ: {cnpj}
+IE: {inscricao_estadual}
+'''
